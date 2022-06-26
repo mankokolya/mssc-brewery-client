6f76b68fbd081c30be9da2d8b15a5c21c122b5f6
Mykola Manko <mankokolya@gmail.com>
2022-06-26 13:38:27 +0300

    add CRUD methods for customer service into the BreweryClient

--- src/test/java/guru/springframework/msscbreweryclient/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -37,5 +38,29 @@ class BreweryClientTest {
     @Test
     void testDeleteDeer() {
         breweryClient.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void testGetCustomerById() {
+        CustomerDto customerDto = breweryClient.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void testSaveNewCustomer() {
+        URI uri = breweryClient.saveNewCustomer(CustomerDto.builder().name("Customer").build());
+        assertNotNull(uri);
+        System.out.println(uri);
+    }
+
+    @Test
+    void testUpdateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        breweryClient.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        breweryClient.deleteCustomer(UUID.randomUUID());
     }
 }
